5802048e7ad5abd657f0a029a8031f6a9e77fac0
khryzmd <dabukhryzmiel@gmail.com>
2024-07-10 09:50:35 +0800

    Add models s51

---
+++ index.js
@@ -0,0 +1,55 @@
+// Dependencies and Modules
+const express = require("express");
+const mongoose = require("mongoose");
+/*const passport = require('passport');
+const session = require('express-session');
+require('./passport.js');
+const cors = require("cors");*/
+const userRoutes = require("./routes/user");
+const productRoutes = require("./routes/product");
+
+// Environment Setup
+require("dotenv").config();
+
+// Server setup
+const app = express();
+
+// Middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+
+/*const corsOptions = {
+	origin: ["http://localhost:8000"],
+	// methods: ["GET", "POST"],
+	// allowedHeaders: ["Content-Type", "Authorization"],
+	credentials: true,
+	// Provides a status code to use for successful OPTIONS request
+	optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));*/
+
+/*// Google Login
+app.use(session({
+	secret: process.env.clientSecret,
+	resave: false,
+	saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());*/
+
+mongoose.connect(process.env.MONGODB_STRING);
+mongoose.connection.once('open', () => console.log("Now connected to MongoDB Atlas"));
+
+
+// Backend Routes
+app.use("/users", userRoutes);
+app.use("/products", productRoutes);
+
+// Server Gateway Response
+if(require.main === module) {
+	app.listen(process.env.PORT || 4000, () => console.log(`API is now available in port ${process.env.PORT || 4000}`));
+}
+
+module.exports = { app, mongoose };
