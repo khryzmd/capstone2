f549c7fc5be2a477b2a4607fec0e509ac25620e3
khryzmd <dabukhryzmiel@gmail.com>
2024-07-29 10:16:08 +0800

    Update index.js backend routes

--- index.js
+++ index.js
@@ -36,10 +36,10 @@ mongoose.connect(process.env.MONGODB_STRING);
 mongoose.connection.once('open', () => console.log("Now connected to MongoDB Atlas"));
 
 // Backend Routes
-app.use("/users", userRoutes);
-app.use("/products", productRoutes);
-app.use("/cart", cartRoutes);
-app.use("/orders", orderRoutes);
+app.use("b3/users", userRoutes);
+app.use("b3/products", productRoutes);
+app.use("b3/cart", cartRoutes);
+app.use("b3/orders", orderRoutes);
 
 // Server Gateway Response
 if(require.main === module) {
